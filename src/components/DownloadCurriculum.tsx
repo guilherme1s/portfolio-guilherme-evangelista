f717d5324250b3a2eb569aec8613aaceabe5e3d1
guilherme1s <2000guilherme.silva@gmail.com>
2026-01-10 20:05:05 -0300

    [FIX] Correção no arquivo de curriculo

--- src/components/DownloadCurriculum.tsx
+++ src/components/DownloadCurriculum.tsx
@@ -3,7 +3,7 @@ import DownloadIcon from "@mui/icons-material/Download";
 export function DownloadCurriculum() {
   return (
     <a
-      href="/Guilherme Evangelista CV.pdf"
+      href="/guilherme-evangelista-cv.pdf"
       download="Guilherme_Evangelista_CV.pdf"
       className="inline-flex items-center gap-2 bg-primary dark:bg-white dark:text-primary hover:text-gray-300 dark:hover:text-dark-secondary text-white font-medium px-4 py-2 rounded-lg transition-colors duration-200"
     >
