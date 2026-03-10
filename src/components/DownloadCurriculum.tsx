211533abd4c61d8b1f983fbc685ca425d76ca869
guilherme1s <2000guilherme.silva@gmail.com>
2026-03-09 21:20:42 -0300

    [MOD] Atualizações links

--- src/components/DownloadCurriculum.tsx
+++ src/components/DownloadCurriculum.tsx
@@ -3,8 +3,8 @@ import DownloadIcon from "@mui/icons-material/Download";
 export function DownloadCurriculum() {
   return (
     <a
-      href="/guilherme-evangelista-cv.pdf"
-      download="Guilherme_Evangelista_CV.pdf"
+      target="__blank"
+      href="https://drive.google.com/file/d/1mj4lhp15-AwI05HVaVhgW1s9EevU-H8z/view"
       className="inline-flex items-center gap-2 bg-primary dark:bg-white dark:text-primary hover:text-gray-300 dark:hover:text-dark-secondary text-white font-medium px-4 py-2 rounded-lg transition-colors duration-200"
     >
       <DownloadIcon />
